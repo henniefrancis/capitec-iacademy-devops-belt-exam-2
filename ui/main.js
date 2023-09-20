79acaf819d148e43146779ad77f7169644682bff
hennie <henniefrancis@gmail.com>
2023-09-20 21:13:40 +0200

    Changes to Solution

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 // Wait for the document to be ready
 $(document).ready(function () {
-    const baseUrl = 'http://localhost:7000/';
+    const baseUrl = 'http://localhost:81/';
     let users = [];
     let activeUser = {};
     // Function to get users from 'api/user' endpoint and display them in the table
